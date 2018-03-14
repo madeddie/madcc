fd290e351a894f3a41884fcd8998b547d53ee222
Edwin Hermans <edwin@madtech.cx>
2018-03-14 15:40:17 -0400

    pytest doesn't support python 3.3 anymore, so neither will I
    Signed-off-by: Edwin Hermans <edwin@madtech.cx>

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Topic :: Utilities',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -39,7 +38,7 @@ setup(
         'requests',
         'tabulate',
     ],
-    python_requires='~=3.3',
+    python_requires='~=3.4',
 
     entry_points={
         'console_scripts': [
