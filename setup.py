024f7cd58a2f2bfe23cb711cbd7cf617d83d79c9
Edwin Hermans <edwin@madtech.cx>
2018-03-20 14:53:45 -0400

    Version bump
    Signed-off-by: Edwin Hermans <edwin@madtech.cx>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='madcc',
-    version='0.0.2-alpha.2',
+    version='0.0.2-alpha.3',
     description='madtech cryptocurrency functions',
     long_description=long_description,
     url='https://github.com/madeddie/madcc',
